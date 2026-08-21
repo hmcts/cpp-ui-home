03a0c9bf609d4007175751d940b528ccdd967878
Paris Freire <paris.freiregallego2@hmcts.net>
2026-08-21 16:33:51 +0100

    GA spike - Update GA4 cookies policy on /cookies page

--- src/app/cookies/cookies.component.ts
+++ src/app/cookies/cookies.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CookiesPreferences, CookiesService } from '@cpp/core';
 import { CookiesPreferencesFormComponent } from './cookies-preferences-form.component';
 import { PdkCore, PdkGrid, PdkTable } from '@cpp/pdk';
+import { Store } from '@ngrx/store';
+import { map } from 'rxjs/operators';
+import { AppState, getAppConfig } from '../app.reducer';
 
 @Component({
   selector: 'app-cookies',
@@ -67,10 +71,10 @@ import { PdkCore, PdkGrid, PdkTable } from '@cpp/pdk';
 
         <h2 pdk-typography="heading-medium">To measure application performance</h2>
         <p pdk-typography="body">
-          We use Dynatrace Software Intelligence Platform to provide an Application Performance
-          Monitoring Service to collect information about how you use HMCTS services. We do this to
-          monitor HMCTS services in order to resolve issues within our services as well as collect
-          data on how our services can be improved. HMCTS store information about:
+          We use Dynatrace Software Intelligence Platform and Google Analytics to collect
+          information about how you use HMCTS services. We do this to monitor HMCTS services in
+          order to resolve issues within our services, understand how the service is being used and
+          to help us make improvements. HMCTS store information about:
         </p>
         <ul pdk-list="bullet">
           <li>Site performance</li>
@@ -80,7 +84,7 @@ import { PdkCore, PdkGrid, PdkTable } from '@cpp/pdk';
         <p pdk-typography="body">
           Information is presented within the Application Performance Monitoring service for the
           purposes detailed above. We do not use or share the information for any other purpose. We
-          do not allow Dynatrace to use or share the information for any other purposes.
+          do not allow Dynatrace or Google to use or share the information for any other purposes.
         </p>
 
         <table class="cookies-table" pdk-table>
@@ -125,6 +129,16 @@ import { PdkCore, PdkGrid, PdkTable } from '@cpp/pdk';
               <td pdk-table-cell>Session timeout</td>
               <td pdk-table-cell>Session end</td>
             </tr>
+            <tr pdk-table-row>
+              <td pdk-table-cell>_ga</td>
+              <td pdk-table-cell>Used to distinguish users</td>
+              <td pdk-table-cell>2 years</td>
+            </tr>
+            <tr pdk-table-row>
+              <td pdk-table-cell>{{ gaSessionCookieName() }}</td>
+              <td pdk-table-cell>Used to persist session state</td>
+              <td pdk-table-cell>2 years</td>
+            </tr>
           </tbody>
         </table>
 
@@ -137,31 +151,30 @@ import { PdkCore, PdkGrid, PdkTable } from '@cpp/pdk';
   `,
   styles: [
     `
-      .cookies-table {
-        table-layout: fixed;
-      }
-      .cookies-table tr > th:nth-child(1),
-      .cookies-table tr > td:nth-child(1) {
-        width: 100px;
-      }
-      .cookies-table tr > th:nth-child(3),
-      .cookies-table > td:nth-child(3) {
-        width: 125px;
-      }
       .cookies-table tr > td:nth-child(1) {
         font-weight: bold;
       }
-    `,
+    `
   ],
-  imports: [CookiesPreferencesFormComponent, PdkGrid, PdkTable, PdkCore],
+  imports: [CookiesPreferencesFormComponent, PdkGrid, PdkTable, PdkCore]
 })
 export class CookiesComponent {
   formValues: CookiesPreferences;
 
+  // _ga_<suffix>'s suffix is the GA4 Measurement ID, not the GTM container ID -
+  // read from its own config field (app.override.config.json) rather than hardcoded.
+  gaSessionCookieName = toSignal(
+    this.store
+      .select(getAppConfig)
+      .pipe(map(appConfig => `_ga_${appConfig?.gaMeasurementId || ''}`)),
+    { initialValue: null }
+  );
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private cookiesService: CookiesService,
-    private router: Router
+    private router: Router,
+    private store: Store<AppState>
   ) {
     this.formValues = this.cookiesService.getAllCookiePreferences();
   }
